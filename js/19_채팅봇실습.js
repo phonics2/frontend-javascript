33d0898bcf765087366346a171c9a76721143680
phonics2 <ha4514@naver.com>
2024-05-22 17:04:32 +0900

    on-click

--- js/19_채팅봇실습.js
+++ js/19_채팅봇실습.js
@@ -1,34 +1,32 @@
-    //자바스크립트는 매개변수가 함수에서 설정한 매개변수와 작성해야하는
-            // 개수가 달라도 문제없이 실행됨
-            보이기("봇입니다.","bot");
-            function 보이기(message,sender){
-                //채팅내용 가져오기
-                const chatbox = document.getElementById("chat");
-                //내용 안에 div 추가하기
-                const messageElement = document.createElement("div");
-                
-                //claasList로   div안에 클래스 추가하기
-                messageElement.classList.add("m",sender+"-m");
-                
-                //채팅 내용 보이기
-                messageElement.textContent = message;
-                
-                //보낸 내용 추가하기
-                chatbox.appendChild(messageElement);
+//자바스크립트는 매개변수가 함수에서 설정한 매개변수와 작성해야하는
+// 개수가 달라도 문제없이 실행됨
+보이기("봇입니다.", "bot");
+function 보이기(message, sender) {
+  //채팅내용 가져오기
+  const chatbox = document.getElementById("chat");
+  //내용 안에 div 추가하기
+  const messageElement = document.createElement("div");
 
-            }
-            function 전송(){
-                
-                const inputbox = document.getElementById("user-input");
-                
-                const message = inputbox.value.trim();
+  //claasList로   div안에 클래스 추가하기
+  messageElement.classList.add("m", sender + "-m");
 
-                if(message == "") return;
-                    
-                보이기(message,"user");
-                inputbox.value = "";
-                //채팅봇 띄우기
-                setTimeout(()=>{
-                    보이기("챗봇입니다.","bot")
-                },1000); //1초후에 메세지 보이기
-            }
+  //채팅 내용 보이기
+  messageElement.textContent = message;
+
+  //보낸 내용 추가하기
+  chatbox.appendChild(messageElement);
+}
+function 전송() {
+  const inputbox = document.getElementById("user-input");
+
+  const message = inputbox.value.trim();
+
+  if (message == "") return;
+
+  보이기(message, "user");
+  inputbox.value = "";
+  //채팅봇 띄우기
+  setTimeout(() => {
+    보이기("챗봇입니다.", "bot");
+  }, 1000); //1초후에 메세지 보이기
+}
--- js/23_onkey-onpress.js
+++ js/23_onkey-onpress.js
@@ -1,9 +1,12 @@
- //toLowerCase() : 영어를 모두 소문자로 변경
+//toLowerCase() : 영어를 모두 소문자로 변경
 //toUpperCase() : 영어를 모두 대문자로 변경
-            function 기능만들기(){
-                const 기능 = document.getElementById("영문이름");
-                기능.value = 기능.value.toUpperCase();
-            }
-            function 엔터기능(){
-                alert("입력한 내용이 전달되었습니다.");
-            }
+function 기능만들기() {
+  const 기능 = document.getElementById("영문이름");
+  기능.value = 기능.value.toUpperCase();
+}
+function 엔터기능(e) {
+  //만약에 엔터 키를 눌렀을 때
+  if (e.key === "Enter") {
+    alert("입력이 완료되었습니다.");
+  }
+}
